c744d85118f1e4938729821b13548c957483a99d
vonHubert <107582269+vonHubert@users.noreply.github.com>
2022-12-22 01:54:56 +0400

    Update NetworkManager.swift

--- SimpleWeather/Services/NetworkManager.swift
+++ SimpleWeather/Services/NetworkManager.swift
@@ -22,29 +22,6 @@ class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
 
-    func fetchWeatherReport(from link: String?, completion: @escaping(Result<MainDataBlock, NetworkError>) -> Void) {
-        guard let url = URL(string: link ?? "") else {
-            completion(.failure(.invalidURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                completion(.failure(.noData))
-                return
-            }
-            
-            do {
-                let weather = try JSONDecoder().decode(MainDataBlock.self, from: data)
-                DispatchQueue.main.async {
-                    completion(.success(weather))
-                }
-            } catch {
-                completion(.failure(.decodingError))
-            }
-        }.resume()
-    }
-    
     func fetchWeatherReportAF(from link: String, completion: @escaping(Result<[WeatherDataBlock], AFError>) -> Void ) {
         AF.request(link)
             .validate()
@@ -53,7 +30,6 @@ class NetworkManager {
                 case .success(let value):
                     let report = MainDataBlock.getWeatherData(from: value)
                     completion(.success(report))
-                    print("REPORT: \(report)")
                 case .failure(let error):
                     completion(.failure(error))
                     
@@ -61,8 +37,5 @@ class NetworkManager {
                 
             }
     }
-    
-    
-    
-    
 }
+
