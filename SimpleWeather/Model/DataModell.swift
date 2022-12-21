f29a3ae422774f7898bf29e1db4a327a1376bd66
vonHubert <107582269+vonHubert@users.noreply.github.com>
2022-12-22 01:54:16 +0400

    Update DataModell.swift

--- SimpleWeather/Model/DataModell.swift
+++ SimpleWeather/Model/DataModell.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MainDataBlock: Decodable {
+struct MainDataBlock {
     let product: String
     let dataseries: [WeatherDataBlock]?
     
@@ -22,50 +22,32 @@ struct MainDataBlock: Decodable {
     }
     
     static func getWeatherData(from value: Any) -> [WeatherDataBlock] {
-       // print("VALUE RECIEVED BY getMainData:\(value)")
         guard let mainData = value as? [String: Any] else {
-      //      print("GUARD ELSE ACTIVATED! value NOT CHECKED AS INITIAL DICTIONARY [String: Any]")
             return []
         }
-      //  print("MAIN DATA: \(mainData)")
         guard let weatherData = mainData["dataseries"] as? [[String: Any]] else {
-      //      print("GUARD ELSE ACTIVATED! value NOT CHECKED AS [[String: Any]]")
             return []
         }
-      //  print("WEATHER DATA: \(mainData)")
         var weatherDataBlock: [WeatherDataBlock] = []
         for weatherDataPacket in weatherData {
-            let pack = WeatherDataBlock(weatherData: weatherDataPacket)
-       //     print("PACK:\(pack)")
-            weatherDataBlock.append(pack)
+            let packet = WeatherDataBlock(weatherData: weatherDataPacket)
+            weatherDataBlock.append(packet)
         }
         return weatherDataBlock
     }
-    
-    
 }
 
-struct WeatherDataBlock: Decodable {
+struct WeatherDataBlock {
     let date: Int
     let weather: String
-    let temp2m: TemperatureDataBlock
+    let temp2m: [String : Any]
     let wind10mMax: Int
     
     init(weatherData: [String: Any]) {
         date = weatherData["date"] as? Int ?? 0
         weather = weatherData["weather"] as? String ?? ""
-        temp2m = weatherData["temp2m"] as? TemperatureDataBlock ?? TemperatureDataBlock(max: 0, min: 0)
+        temp2m = weatherData["temp2m"] as? [String : Any] ?? ["": 0]
         wind10mMax = weatherData["wind10m_max"] as? Int ?? 0
-    }
-}
-
-struct TemperatureDataBlock: Decodable {
-    let max: Int
-    let min: Int
-    
-    init(max: Int, min: Int) {
-        self.max = max
-        self.min = min
     }
 }
 
@@ -73,3 +55,4 @@ struct City {
     let name: String
     let coordinates: [Double]
 }
+
